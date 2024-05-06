8210078266a86677e4726a6ab84a5d40bee7e927
thiagoeu <araujo.thiago1051@gmail.com>
2024-05-06 12:24:18 -0300

    feat: add lighttheme and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
+import { ThemeProvider } from '@mui/material';
+import { LightTheme } from './shared/themes';
+
 
 export const App = () => {
   return (
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
 
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
