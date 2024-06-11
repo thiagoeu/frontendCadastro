161aa137233fa9c25fed016736ba870451a58128
thiagoeu <araujo.thiago1051@gmail.com>
2024-06-11 09:29:52 -0300

    feat: custom hook debounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -3,21 +3,25 @@ import { PageLayoutBase } from "../../shared/layouts";
 import { FerramentasDaListagem } from "../../shared/components";
 import { useSearchParams } from "react-router-dom";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { useDebounce } from "../../shared/hooks";
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000, false);
 
   const busca = useMemo(() => {
     return searchParams.get("busca") || "";
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message);
-      } else {
-        console.log(result.data);
-      }
+    debounce(() => {
+      PessoasService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          console.log(result.data);
+        }
+      });
     });
   }, [busca]);
 
