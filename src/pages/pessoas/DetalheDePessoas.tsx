26a75c93f41e3d20dc2e06bf92baa94dae446a22
thiagoeu <araujo.thiago1051@gmail.com>
2024-06-15 11:50:52 -0300

    feat: DetalheDePessoas

---
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { PageLayoutBase } from "../../shared/layouts";
+import { FerramentasDeDetalhe } from "../../shared/components";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { LinearProgress } from "@mui/material";
+
+export const DetalheDePessoas: React.FC = () => {
+  const { id = "nova" } = useParams<"id">();
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState("");
+
+  useEffect(() => {
+    if (id !== "nova") {
+      setIsLoading(true);
+      PessoasService.getById(Number(id)).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+          navigate("/pessoas");
+        } else {
+          setNome(result.nomeCompleto);
+          console.log(result);
+        }
+      });
+    }
+  }, [id]);
+
+  const handleSave = () => {
+    console.log("save");
+  };
+  const handleDelete = (id: number) => {
+    if (confirm("Realmente deseja apagar?")) {
+      PessoasService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          alert("Registro apagado com sucesso!");
+          navigate("/pessoas");
+        }
+      });
+    }
+  };
+
+  return (
+    <PageLayoutBase
+      titulo={id === "nova" ? "Nova pessoa" : nome}
+      barraDeFerramentas={
+        <FerramentasDeDetalhe
+          textoBotaoNovo="Nova"
+          mostrarBotaoSalvarEFechar
+          mostrarBotaoNovo={id !== "nova"}
+          mostrarBotaoApagar={id !== "nova"}
+          aoClicarEmSalvar={() => {}}
+          aoClicarEmSalvarEFechar={() => {}}
+          aoClicarEmApagar={() => handleDelete(Number(id))}
+          aoClicarEmNovo={() => navigate("/pessoas/detalhe/nova")}
+          aoClicarEmVoltar={() => navigate("/pessoas")}
+        />
+      }
+    >
+      {isLoading && <LinearProgress variant="indeterminate" />}
+      <p>Detalhe de Pessoa {id}</p>
+    </PageLayoutBase>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useDrawerContext } from "../shared/contexts";
 import { useEffect } from "react";
-import { Dashboard, ListagemDePessoas } from "../pages";
+import { Dashboard, DetalheDePessoas, ListagemDePessoas } from "../pages";
 
 export const AppRoutes = () => {
   const { setDrawerOptions } = useDrawerContext();
@@ -25,7 +25,7 @@ export const AppRoutes = () => {
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard />} />
       <Route path="/pessoas" element={<ListagemDePessoas />} />
-      <Route path="/pessoas/detalhe/:id" element={<p>Detalhe</p>} />
+      <Route path="/pessoas/detalhe/:id" element={<DetalheDePessoas />} />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
